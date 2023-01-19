21e0aec7d42c25d9f7e82b76b7f5a03df24d9c5d
kathyavini <kathyavini@gmail.com>
2023-01-19 14:03:07 -0800

    Add controllers for database seeding in e2e tests

--- routes/testing.js
+++ routes/testing.js
@@ -7,12 +7,49 @@ const Post = require('../models/post');
 const Comment = require('../models/comment');
 const User = require('../models/user');
 
+let authorObjId;
+
 testingRouter.post('/reset', async (request, response) => {
   await Post.deleteMany({});
   await Comment.deleteMany({});
   await User.deleteMany({});
 
   response.status(204).end(); // 204 = success, no content
+});
+
+testingRouter.post('/newadminauthor', async (request, response) => {
+  const adminAuthor = new User({
+    displayName: 'Test Admin-Author',
+    username: 'testadminauthor',
+    password: 'testadminauthor',
+    author: true,
+    admin: true,
+    slug: 'test-admin-author',
+  });
+
+  const result = await adminAuthor.save();
+  console.log(result);
+
+  authorObjId = result._id;
+
+  response.status(204).end();
+});
+
+testingRouter.post('/newPost', async (request, response) => {
+  const newPost = new Post({
+    created_at: new Date(),
+    published: true,
+    published_at: new Date(),
+    title: 'Test Post',
+    slug: 'test-post',
+    body: 'Test post content',
+    author: authorObjId,
+  });
+
+  const result = await newPost.save();
+  console.log(result);
+
+  response.status(204).end();
 });
 
 module.exports = testingRouter;
